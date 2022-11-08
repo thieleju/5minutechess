ae43b68209d70f6f183b3ba32b75bb6282c65745
Julian <thiele.juli@gmail.com>
2022-11-08 17:23:35 +0100

    Added vuetify config

--- 5minutechess-frontend/nuxt.config.ts
+++ 5minutechess-frontend/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
---
+++ 5minutechess-frontend/plugins/vuetify.js
@@ -0,0 +1,39 @@
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    ssr: true,
+    components,
+    directives,
+    theme: {
+      defaultTheme: "dark_theme",
+      themes: {
+        dark_theme,
+      },
+      options: { customProperties: true },
+    },
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
+
+const dark_theme = {
+  dark: true,
+  colors: {
+    primary: "#38A3A5",
+    // secondary: "#22577A",
+    secondary: "#0A1013",
+    // background: "#181A1B",
+    background: "#161618",
+    background2: "#2D2D2D",
+    accent: "#80ED99",
+    error: "#FF5252",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FFC107",
+  },
+};
