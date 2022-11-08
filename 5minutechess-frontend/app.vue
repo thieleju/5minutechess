0c5c265fed9ccdcda8e98c70b3608ecc908bb518
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-11-08 17:23:35 +0100

    Added vuetify config

--- 5minutechess-frontend/app.vue
+++ 5minutechess-frontend/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <v-btn color="primary" prepend-icon="mdi-home">Button</v-btn>
+    <v-btn color="secondary" icon="mdi-home"></v-btn>
   </div>
 </template>
