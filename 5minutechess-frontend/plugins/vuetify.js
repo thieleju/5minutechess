596424078cddbfcba3ea74025b65c8a35e327440
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-11-10 22:18:24 +0100

    Added chessinfo component

--- 5minutechess-frontend/plugins/vuetify.js
+++ 5minutechess-frontend/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const dark_theme = {
   dark: true,
