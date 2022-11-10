255dab185d1dd25f7f746c282b99984ea89a4f04
Julian <thiele.juli@gmail.com>
2022-11-10 17:48:48 +0100

    Fixed temporary piece movement

--- 5minutechess-frontend/composables/Board.ts
+++ 5minutechess-frontend/composables/Board.ts
@@ -9,7 +9,23 @@ export default class Board {
   constructor() {
     // create empty board with default fen
     this.apply_fen_to_board(this.fen_start);
-    console.log(this.fields);
+  }
+
+  move(from: Field, to: Field) {
+    // temporary print chess move notation
+    console.log(`move: ${from.piece?.type}${from.notation}${to.notation}`);
+
+    // replace piece on new field with piece on old field
+    this.fields[to.x][to.y].piece = this.fields[from.x][from.y].piece;
+
+    // remove piece from old field
+    this.fields[from.x][from.y] = new Field(from.x, from.y);
+  }
+
+  get_field_by_notation(notation: string): Field {
+    const y = notation.charCodeAt(0) - 97;
+    const x = 8 - parseInt(notation[1]);
+    return this.fields[x][y];
   }
 
   // get empty 2d chessboard array with notation
@@ -48,6 +64,26 @@ export default class Board {
         }
       }
     });
+  }
+
+  get_fen_from_board(): string {
+    let fen = "";
+    for (let x = 0; x < 8; x++) {
+      let empty = 0;
+      for (let y = 0; y < 8; y++) {
+        if (this.fields[x][y].piece) {
+          if (empty) {
+            fen += empty;
+            empty = 0;
+          }
+          let piece = this.fields[x][y].piece;
+          fen += piece?.white ? piece?.type.toUpperCase() : piece?.type;
+        } else empty++;
+      }
+      if (empty) fen += empty;
+      if (x < 7) fen += "/";
+    }
+    return fen;
   }
 
   toString() {
