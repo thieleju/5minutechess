2b681b595e47198f6f587219ca019713bb86d5e5
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-11-11 13:32:58 +0100

    Moved server routes in subfolders

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,16 @@
 
 <template>
   <v-main>
-    <client-only placeholder="Loading...">
-      <v-row align="center">
-        <v-col cols="12" md="9" sm="12" class="mr-md-2 mr-sm-auto container">
-          <chessboard></chessboard>
-        </v-col>
-        <v-col cols="12" md="3" sm="12" class="ml-md-2 ml-sm-auto container">
-          <chessinfo></chessinfo>
-        </v-col>
-      </v-row>
-    </client-only>
+    <!-- <client-only placeholder="Loading..."> -->
+    <v-row align="center">
+      <v-col cols="12" md="9" sm="12" class="mr-md-2 mr-sm-auto container">
+        <chessboard></chessboard>
+      </v-col>
+      <v-col cols="12" md="3" sm="12" class="ml-md-2 ml-sm-auto container">
+        <chessinfo></chessinfo>
+      </v-col>
+    </v-row>
+    <!-- </client-only> -->
   </v-main>
 </template>
 
