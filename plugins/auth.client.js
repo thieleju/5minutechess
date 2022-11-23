48312d897d846f8bde04a2414bec108dcd3a467a
Julian <thiele.juli@gmail.com>
2022-11-23 20:59:33 +0100

    Fixed automatic logout

--- plugins/auth.client.js
+++ plugins/auth.client.js
@@ -1,21 +1,44 @@
 // This Plugin executes only on the client
 export default defineNuxtPlugin((nuxtApp) => {
   // try to autologin from local storage
-  // read state from local storage
   const state_user = useStateUser();
+  // read state from local storage
   const storage_string = localStorage.getItem("state_user");
 
+  function doSimpleLogout() {
+    localStorage.removeItem("state_user");
+    console.log(`Autologin failed!`);
+  }
+
   if (!storage_string) {
-    console.log("Autologin failed");
+    doSimpleLogout();
     return;
   }
 
-  state_user.value = JSON.parse(storage_string);
+  // check stored token validity
+  const storage = JSON.parse(storage_string);
 
-  console.log(
-    `Autologin for user ${state_user.value.username} from localStorage`
-  );
+  $fetch("/api/auth/check_token", {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${storage.jwt}`,
+    },
+    method: "get",
+  })
+    .then((res) => {
+      if (res.status !== "ok") {
+        doSimpleLogout();
+        return;
+      }
 
-  // not working atm
-  // nuxtApp.provide("$logout", () => localStorage.removeItem("state_user"));
+      // token still valid
+      state_user.value = storage;
+
+      console.log(
+        `Autologin for user ${state_user.value.username} from localStorage`
+      );
+    })
+    .catch((err) => {
+      console.log(`Check token error`, storage_string, err);
+    });
 });
